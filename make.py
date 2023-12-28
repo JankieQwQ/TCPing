0b785f22414644c5cdb800921740f38a0df1af46
Jankie <notflyloong@outlook.com>
2023-12-28 18:41:20 +0800

    Remove colorama and use ASCII color keywords

--- make.py
+++ make.py
@@ -1,18 +1,7 @@
 import os
-
-coloramaFlag = True
-try:
-    import colorama
-except ModuleNotFoundError:
-    coloramaFlag = False
-
 if __name__ == '__main__':
     output_name = 'tcping{}'.format(os.name.replace('nt','.exe').replace('posix',''))
     code = os.system('g++ -o {} main.cpp'.format(output_name))
-    failed = '[Failed] An error was thrown while compiling with g++, please check the command line output'
-    if code:
-        if coloramaFlag:
-            print(colorama.Fore.RED + failed)
-        else:
-            print(failed)
+    failed = '[Failed] An error was thrown while compiling with g++, please check the command line output.'
+    print('\033[0;31;40m{}\033[0m'.format(failed))
     raise SystemExit(0)
