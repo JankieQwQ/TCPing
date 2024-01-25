8ba4eb14cda7c57b5657002b3640c84ae1b5caa6
Jankie <qwq@awaish.link>
2024-01-25 20:14:54 +0800

    Change path

--- make.py
+++ make.py
@@ -1,7 +1,7 @@
 import os
 if __name__ == '__main__':
     output_name = 'tcping{}'.format(os.name.replace('nt','.exe').replace('posix',''))
-    code = os.system('g++ -o {} main.cpp'.format(output_name))
+    code = os.system('g++ -o {} ./src/main.cpp'.format(output_name))
     failed = '[Failed] An error was thrown while compiling with g++, please check the command line output.'
     print('\033[0;31;40m{}\033[0m'.format(failed))
     raise SystemExit(code)
